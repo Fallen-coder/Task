1f5acb38081d790f75156ba7ef7f5f2cabf5aea2
Your Name <you@example.com>
2025-09-16 15:14:29 +0300

    opp

--- uzd.php
+++ uzd.php
@@ -1,5 +1,28 @@
 <?php
-$tasks = ["ghgf","sdfgfd","uiuii"];
+class Task{
+    public $task;
+
+    public function __construct($task){
+        $this->task=$task;
+    }
+    public function show(){
+        echo "Task: $this->task \n";
+    }
+    public function __destruct(){
+        echo "task deleted --- Task: $this->task\n";
+    }
+    public function edit($edit){
+        $this->task =$edit;
+    }
+    public function add($add){
+        $tasks[] =new Task($add);
+    }
+}
+$tasks = [
+    new Task("ghgf"),
+    new Task("sdfgfd"),
+    new Task("uiuii"),
+];
 
 function line(){
     echo "---------------------------\n\n";
@@ -7,32 +30,30 @@ function line(){
 
 
 function view(&$tasks){
-    $id=1;
     foreach($tasks as $task){
-        echo $id.". ".$task."\n";
-        $id+=1;
+        $task->show();
     }
 }
-function add($tasks){
+function add(&$tasks){
     $add = readline("add===>>>  ");
-    $tasks[] =$add;
+    $task->add($add);
 }
 function destroy(&$tasks){
     $id = readline("id===>>>  ");
     unset($tasks[$id-1]);
     $tasks = array_values($tasks);
 }
-function update(&$tasks){
-    $id = readline("id===>>>  ");
-    $edit = readline("edit===>>>  ");
-    $tasks[] = $edit;
+function main(){
+    echo "Task\n";
+    echo "1. view all\n";
+    echo "2. view one\n";
+    echo "3. add\n";
+    echo "4. destroy\n";
+    echo "5. edit\n";
+    echo "6. help\n";
+    line();
 }
-echo "Task";
-echo "1. view all\n";
-echo "2. add\n";
-echo "3. destroy\n";
-echo "4. edit\n";
-line();
+main();
 while (true){
     
     $input = readline("input===>>>  ");
@@ -46,16 +67,26 @@ while (true){
             line();
             break;
         case '2':
+            $id = readline("what task(id)===>>>  ");
+            $tasks[$id-1]->show();
+            line();
+        break;
+        case '3':
             add($tasks);
             line();
             break;
-        case '3':
+        case '4':
             destroy($tasks);
             line();
             break;
-        case '4':
-            update();
+        case '5':
+            $id = readline("id===>>>  ");
+            $edit = readline("edit===>>>  ");
+            $tasks[$id-1]->edit($edit);
             line();
+        break;
+        case 'help':
+            main();
         break;
                                                       
         default:
