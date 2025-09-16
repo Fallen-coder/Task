ec9308dce15dd7ef623143114b2bc07271ded3a0
Your Name <you@example.com>
2025-09-16 14:03:41 +0300

    better disine

--- uzd.php
+++ uzd.php
@@ -1,6 +1,11 @@
 <?php
 $tasks = ["ghgf","sdfgfd","uiuii"];
 
+function line(){
+    echo "---------------------------\n\n";
+}
+
+
 function view(&$tasks){
     $id=1;
     foreach($tasks as $task){
@@ -22,27 +27,40 @@ function update(&$tasks){
     $edit = readline("edit===>>>  ");
     $tasks[] = $edit;
 }
+echo "Task";
+echo "1. view all\n";
+echo "2. add\n";
+echo "3. destroy\n";
+echo "4. edit\n";
+line();
 while (true){
+    
     $input = readline("input===>>>  ");
+    line();
     if ($input == "n"){
         exit;
     }
     switch ($input) {
         case '1':
             view($tasks);
+            line();
             break;
         case '2':
             add($tasks);
+            line();
             break;
         case '3':
             destroy($tasks);
+            line();
             break;
         case '4':
             update();
+            line();
         break;
                                                       
         default:
             echo "no such function \n";
+            line();
             break;
     }
 
