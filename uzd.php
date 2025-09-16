f3de8e46e4c84195b443f29e4c3468c8613650b2
Your Name <you@example.com>
2025-09-16 13:20:37 +0300

    fuction 1 and 2 errors fixed

--- uzd.php
+++ uzd.php
@@ -1,9 +1,27 @@
 <?php
-$task = array("ghgf","sdfgfd","uiuii");
+$tasks = ["ghgf","sdfgfd","uiuii"];
+
+function view(&$tasks){
+    $id=1;
+    foreach($tasks as $task){
+        echo $id.". ".$task."\n";
+        $id+=1;
+    }
+}
 
 while (true){
     $input = readline("input===>>>  ")."\n";
-    if ($input = "n"){
+    if ($input == "n"){
         exit;
     }
+    switch ($input) {
+        case '1':
+            view($tasks);
+            break;
+        
+        default:
+            echo "no such function \n";
+            break;
+    }
+
 }
